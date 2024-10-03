36b3e6c4a51478364ce30ff66083ae503e9a7d86
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2024-10-03 17:01:25 -0300

    Create producao.exs

---
+++ producao.exs
@@ -0,0 +1,17 @@
+defmodule Producao do
+	@doc "Inicia o registro da producao"
+	def iniciar() do
+	end
+
+	@doc "obtem o total de biscoitos produzidos"
+	def total_produzidos() do
+	end
+
+	@doc """obtem a media de biscoitos produzidos por fornada"""
+	def media_produzidos() do
+	end
+
+	@doc "adiciona uma nova fornada de biscoitos"
+	def adiciona_producao(quantidade) do
+	end
+end
