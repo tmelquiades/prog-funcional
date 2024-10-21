007d255e19c0a4a30e4bb6270a9cafe56c8184a1
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2024-10-21 19:30:53 -0300

    Update http.ex

--- projeto_final/http.ex
+++ projeto_final/http.ex
@@ -1,58 +1,28 @@
-prog func
-
 defmodule Http do
 	def servidor(porta) do
 		# criar o socket
 		{ :ok, socket } = :gen_tcp.listen(porta, active: false)
+    
 		# esperar conexões dos clientes
-		IO.puts "Esperando por conexão na porta #{porta}"
+    IO.puts "=== Esperando conexão na porta #{porta}"
+    recebe_conexao(socket)
+    
+    # fechar socket
+    :gen_tcp.close(socket)
+  end
+  
+  def recebe_conexao(socket) do
+    IO.puts "Esperando por conexão na porta #{porta}"
 		{ :ok, cliente } = :gen_tcp.accept(socket)
 
 		# receber o pacote com a requisição
 		{ :ok, pacote } = :gen_tcp.recv(cliente, 0)
-		IO.puts("=== Dados recebidos :")
-		IO.inspect(analisa_req(pacote))
-
-		# enviar a resposta
-		resposta(cliente)
-		IO.puts("=== Resposta enviada, fechando conexão")
-		# fechar sockets
-		:gen_tcp.close(cliente)
-		:gen_tcp.close(socket)
-	end
-
-	defp analisa_req(pacote) do
-		String.split(to_string(pacote), "\r\n")
-		|> List.first
-		|> String.split
-		|> Enum.take(2)
-		|> List.to_tuple
+		resposta = Servidor.gera_resposta(pacote)
+    
+    # enviar a resposta
+    :gen_tcp.send(cliente, resposta)
+    IO.puts("=== Resposta enviada, fechando conexão")
+    
+    :gen_tcp.close(cliente)
   end
-
-	@resp [ "HTTP/1.1 200 OK",
-		"Content-Type: text/html; charset=utf-8",
-		"Content-Length: 0",
-		""]
-	@index ["<!DOCTYPE html>",
-		"<html>",
-		"<head>"
-		"	<title>Oi</title>",
-		"<meta charset=\"utf-8\"/>",
-		"</head>",
-		"<body>",
-		"<h1>Olá</h1>",
-		"</body>",
-		"</html>"]
-
-	def cria_resposta(conteudo) do
-		corpo = Enum.join(conteudo, "\r\n")
-		conlen = "Content-Length: #{byte_size corpo}"
-		Enum.join(@resp, "\r\n") <> conlen <> "\r\n\r\n" <> corpo
-	end
-
-	def resposta(cliente) do
-		resp = Enum.join(@resp, "\r\n")
-		:gen_tcp.send(cliente, resp)
-	end
 end
-		
